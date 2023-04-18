ed53fd38a53c920c4ee4c3caa9c94daddc5d424e
Richard Nader <rnader@dyn-it.com>
2023-04-18 13:45:51 +0000

    Admin Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+		$created_at = Carbon::now();
+        $user_id = DB::table('users')->insert([
+            'name' => 'administrator',
+			'password' => Hash::make('administrator'),
+            'email' => 'admin@dyn-it.com',
+            'sys_admin' => true,
+            'enabled' => true,
+        //    'theme' => 'default',
+            'created_at' => $created_at,
+            'updated_at' => $created_at,
+            'email_verified_at' => $created_at
+        ]);
+
     }
 }

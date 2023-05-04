0113cfb564b1058e305e9d18972e33c587e6a4b1
Richard Nader <rnader@dyn-it.com>
2023-05-04 10:03:07 +0000

    hotfix

--- resources/views/help/income.blade.php
+++ resources/views/help/income.blade.php
@@ -6,11 +6,11 @@
 
 <p>Meaningful fields are encrypted in the database for your protection.  These are indicated above.</p>
 
-@if($mode =='show') {
+@if($mode =='show')
     <h5>Special Controls</h5>
     <ul>
         <li>[ <i class='fa-solid fa-fw fa-rotate'></i>&nbsp;&nbsp;Cycle ]: This will 'cycle' the expense, create a register record, and advance the date according to the frequency setting.</li>
     </ul>
-}
+@endif
 
 @include( 'help.controls')
